3f1428fc939732fea305aa5433b3c78d895297df
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-03-30 23:47:13 +0700

    remake python check script, add functions

--- check_matrix.py
+++ check_matrix.py
@@ -1,46 +1,20 @@
-import sys
-
 import numpy
 
-
-def fill_matrix_2(height, width, matrix):
-    k = 1.0
-    for i in range(0, height):
-        tmp_matr = []
-        for j in range(0, width):
-            tmp_matr.append(k)
-            k += 1.0
-        matrix[i] = (tmp_matr)
-
-
-def fill_matrix_1(height, width, matrix):
-    for i in range(0, height):
-        tmp_matr = []
-        for j in range(0, width):
-            if i == j:
-                tmp_matr.append(2.0)
-            else:
-                tmp_matr.append(1.0)
-        matrix[i] = (tmp_matr)
-
-
-argv = sys.argv
-n1, n2, n3, type = map(int, argv[1::])
-
-matrix1 = [[]] * n1
-matrix2 = [[]] * n2
-
-if type == 1:
-    fill_matrix_1(n1, n2, matrix1)
-    fill_matrix_1(n1, n2, matrix2)
-if type == 2:
-    fill_matrix_2(n1, n2, matrix1)
-    fill_matrix_2(n1, n2, matrix2)
-
-matr_mul = (numpy.matmul(matrix1, matrix2))
-file = open("matrix.txt", "r")
+file = open("matrix_A.txt", "r")
 str = file.read()
-c_matrix = eval(str)
-c_matrix = numpy.array(c_matrix)
+A_matrix = eval(str)
+A_matrix = numpy.array(A_matrix)
 
-print((c_matrix == matr_mul).all())
+file = open("matrix_B.txt", "r")
+str = file.read()
+B_matrix = eval(str)
+B_matrix = numpy.array(B_matrix)
+
+
+matr_mul = (numpy.matmul(A_matrix, B_matrix))
+file = open("matrix_C.txt", "r")
+str = file.read()
+C_matrix = eval(str)
+C_matrix = numpy.array(C_matrix)
+
+print((C_matrix == matr_mul).all())
--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <unistd.h>
-#include <string.h>
 
-#define N1 21
-#define N2 21
-#define N3 21
+#define N1 100
+#define N2 100
+#define N3 100
 
 #define RANK_ROOT 0
+#define X 0
+#define Y 1
 
-void fill_matrix_1(double *A, int height, int width) {
+void fill_matrix(double *A, int height, int width) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (i == j)
@@ -20,15 +20,16 @@ void fill_matrix_1(double *A, int height, int width) {
         }
     }
 }
-void fill_matrix_2(double *A, int height, int width) {
-    for (int i = 0; i < height * width; i++) {
-        A[i] = i + 1;
-    }
-}
+
+//void fill_matrix(double *A, int height, int width) {
+//    for (int i = 0; i < height * width; i++) {
+//        A[i] = i + 1;
+//    }
+//}
 
 
-void file_print_matrix(double *A, int height, int width) {
-    FILE *file = fopen("./matrix.txt", "w");
+void file_print_matrix(double *A, int height, int width,char* file_name) {
+    FILE *file = fopen(file_name, "w");
     fprintf(file, "[");
     for (int i = 0; i < height; i++) {
         fprintf(file, "[");
@@ -42,23 +43,10 @@ void file_print_matrix(double *A, int height, int width) {
     fclose(file);
 }
 
-void print_matrix(double *A, int height, int width) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%lf ", A[i * width + j]);
-        }
-        puts("\n");
-    }
-}
-
-
 void
 matrix_multiply(const double *A, int A_height, int A_width, const double *B, int B_height, int B_width, double *C) {
     if (A_width != B_height)
         return;
-
-   // memset(C, 0, sizeof(*C) * A_height * B_width);
-
     for (int i = 0; i < A_height; i++) {
         for (int k = 0; k < A_width; k++) {
             for (int j = 0; j < B_width; j++) {
@@ -68,12 +56,8 @@ matrix_multiply(const double *A, int A_height, int A_width, const double *B, int
     }
 }
 
-#define X 0
-#define Y 1
-
 MPI_Comm create_new_comm(int size, int rank, int *dims) {
     int periods[2] = {0, 0}, coords[2], reorder = 1;
-
     int sizey, sizex;
     MPI_Comm comm2d;
 
@@ -101,10 +85,10 @@ send_matrix(double *A_recv, double *B_recv, int A_split_size, int B_split_size, 
     MPI_Type_create_resized(column, 0, (int) (B_split_size * sizeof(double)), &column_resized);
     MPI_Type_commit(&column_resized);
 
-    if (coords[0] == 0) {
+    if (coords[X] == 0) {
         MPI_Scatter(A, N2 * A_split_size, MPI_DOUBLE, A_recv, N2 * A_split_size, MPI_DOUBLE, 0, col_comm);
     }
-    if (coords[1] == 0) {
+    if (coords[Y] == 0) {
         MPI_Scatter(B, 1, column_resized, B_recv, N2 * B_split_size, MPI_DOUBLE, 0, row_comm);
     }
     MPI_Barrier(MPI_COMM_WORLD);
@@ -117,59 +101,8 @@ send_matrix(double *A_recv, double *B_recv, int A_split_size, int B_split_size, 
 
 }
 
-int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
-    double start = MPI_Wtime();
-    int world_size, world_rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    double *A, *B, *C;
-    if (world_rank == RANK_ROOT) {
-        A = calloc(N1 * N2, sizeof(double));
-        fill_matrix_2(A, N1, N2);
-        B = calloc(N2 * N3, sizeof(double));
-        fill_matrix_2(B, N2, N3);
-        C = calloc(N1 * N3, sizeof(double));
-    }
-    int dims[2] = {0, 0}; //x y
-
-    MPI_Comm comm2d = create_new_comm(world_size, world_rank, dims);
-    int periods[2] = {0, 0}, coords[2];
-    MPI_Cart_get(comm2d, 2, dims, periods, coords);
-    MPI_Comm row_comm = NULL, col_comm = NULL; //x y
-    MPI_Comm_split(comm2d, coords[0], coords[1], &col_comm);
-    MPI_Comm_split(comm2d, coords[1], coords[0], &row_comm);
-    MPI_Barrier(comm2d);
-    int row_rank, row_size;
-    MPI_Comm_rank(row_comm, &row_rank);
-    MPI_Comm_size(row_comm, &row_size);
-    int col_rank, col_size;
-    MPI_Comm_rank(col_comm, &col_rank);
-    MPI_Comm_size(col_comm, &col_size);
-
-    if (row_size != dims[0] || col_size != dims[1]) {
-        perror("Bad size of topology");
-        exit(1);
-    }
-    if (N1 % col_size != 0 || N3 % row_size != 0) {
-        perror("Bad size of matrix");
-        exit(1);
-    }
-
-    int A_split_size = N1 / dims[1];
-    int B_split_size = N3 / dims[0];
-    double *A_recv = calloc(A_split_size * N2, sizeof(double));
-    double *B_recv = calloc(B_split_size * N2, sizeof(double));
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    send_matrix(A_recv, B_recv, A_split_size, B_split_size, A, B, coords, row_comm, col_comm);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    double *C_part = calloc(A_split_size * B_split_size, sizeof(double));
-    matrix_multiply(A_recv, A_split_size, N2, B_recv, N2, B_split_size, C_part);
-
-
+void receive_matrix(int A_split_size, int B_split_size, int world_rank, int world_size, const int *dims, double *C,
+                    const double *C_part, MPI_Comm comm2d) {
     MPI_Datatype send_matrix_part, send_matrix_part_resized;
     MPI_Type_vector(A_split_size, B_split_size, N3, MPI_DOUBLE, &send_matrix_part);
     MPI_Type_commit(&send_matrix_part);
@@ -180,36 +113,95 @@ int main(int argc, char **argv) {
     if (world_rank == RANK_ROOT) {
         displs = calloc(world_size, sizeof(int));
         sizes = calloc(world_size, sizeof(int));
-        for (int i = 0; i < dims[1]; i++) {
-            for (int j = 0; j < dims[0]; j++) {
-                displs[i + j * dims[1]] = (j * B_split_size + A_split_size * i * N3) / B_split_size;
+        for (int i = 0; i < dims[Y]; i++) {
+            for (int j = 0; j < dims[X]; j++) {
+                displs[i + j * dims[Y]] = (j * B_split_size + A_split_size * i * N3) / B_split_size;
             }
         }
         for (int i = 0; i < world_size; i++) {
             sizes[i] = 1;
         }
     }
-    MPI_Gatherv(C_part, B_split_size * A_split_size, MPI_DOUBLE, C, sizes, displs, send_matrix_part_resized, RANK_ROOT, comm2d);
+    MPI_Gatherv(C_part, B_split_size * A_split_size, MPI_DOUBLE, C, sizes, displs, send_matrix_part_resized, RANK_ROOT,
+                comm2d);
     MPI_Type_free(&send_matrix_part);
     MPI_Type_free(&send_matrix_part_resized);
+    if (world_rank == RANK_ROOT) {
+        free(displs);
+        free(sizes);
+    }
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    double *A, *B, *C;
+    if (world_rank == RANK_ROOT) {
+        A = calloc(N1 * N2, sizeof(double));
+        fill_matrix(A, N1, N2);
+        file_print_matrix(A, N1, N2,"./matrix_A.txt");
+        B = calloc(N2 * N3, sizeof(double));
+        fill_matrix(B, N2, N3);
+        file_print_matrix(B, N2, N3,"./matrix_B.txt");
+        C = calloc(N1 * N3, sizeof(double));
+    }
+
+    double start = MPI_Wtime();
+
+    int dims[2] = {0, 0}, periods[2] = {0, 0}, coords[2]; //x y
+    MPI_Comm comm2d = create_new_comm(world_size, world_rank, dims);
+
+    MPI_Cart_get(comm2d, 2, dims, periods, coords);
+    MPI_Comm row_comm = NULL, col_comm = NULL; //x y
+    MPI_Comm_split(comm2d, coords[0], coords[1], &col_comm);
+    MPI_Comm_split(comm2d, coords[1], coords[0], &row_comm);
+
+    int row_rank, row_size;
+    MPI_Comm_rank(row_comm, &row_rank);
+    MPI_Comm_size(row_comm, &row_size);
+    int col_rank, col_size;
+    MPI_Comm_rank(col_comm, &col_rank);
+    MPI_Comm_size(col_comm, &col_size);
+
+    if (row_size != dims[X] || col_size != dims[Y]) {
+        perror("Bad size of topology");
+        exit(1);
+    }
+    if (N1 % col_size != 0 || N3 % row_size != 0) {
+        perror("Bad size of matrix");
+        exit(1);
+    }
+
+    int A_split_size = N1 / dims[Y];
+    int B_split_size = N3 / dims[X];
+    double *A_recv = calloc(A_split_size * N2, sizeof(double));
+    double *B_recv = calloc(B_split_size * N2, sizeof(double));
+
+    send_matrix(A_recv, B_recv, A_split_size, B_split_size, A, B, coords, row_comm, col_comm);
+
+    double *C_part = calloc(A_split_size * B_split_size, sizeof(double));
+    matrix_multiply(A_recv, A_split_size, N2, B_recv, N2, B_split_size, C_part);
+
+    receive_matrix(A_split_size, B_split_size, world_rank, world_size, dims, C, C_part, comm2d);
 
     double end = MPI_Wtime();
 
     if (world_rank == RANK_ROOT) {
-        file_print_matrix(C, N1, N3);
+        file_print_matrix(C, N1, N3,"./matrix_C.txt");
         char script[100];
-        sprintf(script, "/mnt/c/'Python 3.8.2'/python.exe ./check_matrix.py %d %d %d %d", N1, N2, N3, 2);
-        if( system(script)!= 0 ) {
+        sprintf(script, "/mnt/c/'Python 3.8.2'/python.exe ./check_matrix.py");
+        if (system(script) != 0) {
             perror("Script didn't run");
         }
         printf("%lf sec\n", end - start);
         free(A);
         free(B);
         free(C);
-        free(displs);
-        free(sizes);
     }
-
+    free(A_recv);
+    free(B_recv);
     MPI_Finalize();
     return 0;
 }
